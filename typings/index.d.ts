3aab422162daea5f8b1f504d169b2cd160339676
fengqinglingyu <liang823740098@hotmail.com>
2019-04-22 17:59:44 +0800

    Update index.d.ts
    Change egg-mysql typing

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,69 +1,9 @@
-import 'egg';
-declare module 'egg' {
-  interface ISelectConfig {
-    where?: object;
-    columns?: string[];
-    orders?: string[][];
-    limit?: number;
-    offset?: number;
-  }
+import "egg";
+import mysql from "./plugins/mysql";
 
-  interface IQueryResult {
-    fieldCount: number;
-    affectedRows: number;
-    insertId: number;
-    serverStatus: number;
-    warningCount: number;
-    message: string;
-    protocol41: boolean;
-    changedRows: number;
-  }
-
-  interface mysql {
-    /**
-     *
-     * @param tableName 表名
-     * @param config 配置
-     */
-    get(tableName: string, config: any): Promise<any>;
-
-    /**
-     *
-     * @param tableName 表名
-     * @param config 配置
-     */
-    select(tableName: string, config?: ISelectConfig): Promise<any>;
-
-    /**
-     *
-     * @param queryString SQL查询的字符串
-     * @param params mysql.escape的参数
-     */
-    query(queryString: string, params?: any[]);
-
-    /**
-     * 往表里面插入数据
-     * @param tableName 表名
-     * @param values 需要插入的数据
-     */
-    insert(tableName: string, values: object);
-
-    /**
-     * 更新表数据
-     * @param tableName 表名
-     * @param values 需要更新的数据
-     */
-    update(tableName: string, values: object);
-    /**
-     * 获取MySQL内建方法
-     */
-    literals: any;
-
-    // mysql 执行查询的结果
-    queryResult: IQueryResult;
-  }
-
+declare module "egg" {
   interface Application {
     mysql: mysql;
   }
 }
+
